51be3987bf1ec95ef7dd618e5713945a12933bec
Mateo Giraz <mateogiraz27@gmail.com>
2024-09-26 23:48:51 -0300

    avoid overriding default config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,43 +1,38 @@
 local wezterm = require("wezterm")
 
-config = wezterm.config_builder()
+local config = wezterm.config_builder()
 
-config = {
-  automatically_reload_config = true,
-  enable_tab_bar = false,
-  window_close_confirmation = "NeverPrompt",
-  window_decorations = "RESIZE", 
-  default_cursor_style = "BlinkingBar",
-  color_scheme = "Nord (Gogh)",
-  font = wezterm.font("JetBrains Mono"),
-  font_size = 15,
-  keys = {
-    {key="v", mods="CTRL", action=wezterm.action{PasteFrom="Clipboard"}},
-    {key="c", mods="CTRL", action=wezterm.action{CopyTo="Clipboard"}},
-    {key = 'f', mods = 'SHIFT|CTRL', action = wezterm.action.ToggleFullScreen},
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.default_cursor_style = "BlinkingBar"
+config.color_scheme = "Nord (Gogh)"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 15
+
+config.background = {
+  {
+    source = {
+      File = "/Users/mateogiraz/Pictures/wallpaper.jpg"
+    },
+    hsb = {
+      hue = 1.0,
+      saturation = 1.02,
+      brightness = 0.25,
+    },
+    width = "100%",
+    height = "100%",
   },
-  background = {
-    {
-      source = {
-        File = "/Users/mateogiraz/Pictures/wallpaper.jpg"
-      },
-      hsb = {
-        hue = 1.0,
-        saturation = 1.02,
-        brightness = 0.25,
-      },
-      width = "100%",
-      height = "100%",
+  {
+    source = {
+      Color = "#282c35",
     },
-    {
-      source = {
-        Color = "#282c35",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.55,
-    },
+    width = "100%",
+    height = "100%",
+    opacity = 0.55,
   },
 }
 
 return config
+
